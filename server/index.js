d0cea2c2cd539231217ef016d1f3773e04f8406e
Devaraj <contact@deveraj.me>
2024-12-20 10:20:09 +0530

    SSL enabled

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cors from "cors";
+import http from "http";
+import https from "https";
+import fs from "fs";
 
 dotenv.config();
 const app = express();
@@ -10,10 +13,17 @@ app.use(express.static("./dist"));
 
 app.get("/data", (req, res) => res.json({ msg: "Success" }));
 // Routers
-
 app.all("/*", (req, res) => res.sendFile(path.resolve("./dist/index.html")));
 
-app.listen(process.env.VITE_PORT, error => {
-    if(error) return console.log(error);
-    console.log(`Server started on port ${process.env.VITE_PORT}`);
-});
+const options = {
+    key: fs.readFileSync("./ssl/private.key"),
+    cert: fs.readFileSync("./ssl/certificate.crt")
+}
+
+http.createServer(app).listen(VITE_PORT_HTTP, e => console.log(e ? e : "HTTP server running..."));
+https.createServer(options, app).listen(VITE_PORT_HTTPS, e => console.log(e ? e : "HTTPS server running..."));
+
+// app.listen(process.env.VITE_PORT, error => {
+//     if(error) return console.log(error);
+//     console.log(`Server started on port ${process.env.VITE_PORT}`);
+// });
